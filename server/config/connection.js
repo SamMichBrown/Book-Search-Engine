0a58ffc0bc3681feb0044025bceae3c8c8a08499
Samantha Brown <71106177+SamMichBrown@users.noreply.github.com>
2021-05-02 19:30:47 -0400

    error fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Book-Search-Engine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search-engine-app', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
