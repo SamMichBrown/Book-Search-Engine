dac7915050781c8d9aad78e59948421713cd0bea
Samantha Brown <71106177+SamMichBrown@users.noreply.github.com>
2021-05-09 12:23:30 -0400

    fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 console.log("-------", process.env.MONGODB_URI)
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Book-Search-Engine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search-engine', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: true
+  useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
