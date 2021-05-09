e32730b4a3c08265d2b0181b8afcf1bc2820158d
Samantha Brown <71106177+SamMichBrown@users.noreply.github.com>
2021-05-08 22:09:20 -0400

    fixes to heroku deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 console.log("-------", process.env.MONGODB_URI)
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/BOOK-SEARCH-ENGINE', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search-engine', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
