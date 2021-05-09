e3e9c41a53f82a2fd29ff28acbd7abf60f51406c
Samantha Brown <71106177+SamMichBrown@users.noreply.github.com>
2021-05-09 11:52:32 -0400

    minor fix again

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 console.log("-------", process.env.MONGODB_URI)
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search-engine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/hidden-scrubland-75569', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
